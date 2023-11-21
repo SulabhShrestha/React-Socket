e2f012f89f19f4d21d1664423a906da2a2e7730a
Sulabh Shrestha <shresthasulabh777@gmail.com>
2023-11-21 23:31:04 +0545

    Message sent and received can be displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { io } from "socket.io-client";
+import MessageModel from "./model/message_model";
 
 function App() {
   const socket = io("http://localhost:3000");
 
+  const [messages, setMessages] = useState([]);
+
   // connection socket
   socket.on("connect", () => {
     console.log("Socket connected");
+  });
+
+  // receive message
+  socket.on("message", (message) => {
+    console.log("Message received: ", message);
+    setMessages([...messages, message]);
   });
 
   // disconnect socket
@@ -21,12 +30,24 @@ function App() {
     e.preventDefault();
     const message = formRef.current[0].value;
     if (message !== "") {
-      socket.emit("message", message);
+      socket.emit("message", new MessageModel(socket.id, new Date(), message));
       formRef.current[0].value = "";
+      setMessages([
+        ...messages,
+        new MessageModel(socket.id, new Date(), message),
+      ]);
     }
   };
   return (
     <>
+      <div className="messages">
+        {messages.map((message, index) => (
+          <p key={index}>
+            Sent by: {message.sender} at {message.date.toLocaleString()}{" "}
+            {message.msg}
+          </p>
+        ))}
+      </div>
       <form action="" ref={formRef} onSubmit={sendMessage}>
         <input type="text" name="" id="" placeholder="Enter a message" />
         <input type="submit" value="Send" />
---
+++ src/model/message_model.js
@@ -0,0 +1,9 @@
+class MessageModel {
+  constructor(sender, date, msg) {
+    this.sender = sender;
+    this.date = date;
+    this.msg = msg;
+  }
+}
+
+export default MessageModel;
